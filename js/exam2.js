0548b011ed300c588ce521af4268a965f471b8ae
Arden Stirling <ardenstirling@Ardens-MacBook-Pro-93.local>
2026-05-25 13:06:18 +0100

    lightbox

--- js/exam2.js
+++ js/exam2.js
@@ -39,41 +39,15 @@ document.querySelector(".styled2").addEventListener("click",function(){
     parent.style.backgroundImage = "url('images/snowy-peak.jpg')";
 })
 
-// adds lightbox to exam2.js //
+// tried to add data-lightbox attribute to each img? //
 
-function openModal() {
-  document.getElementById("myModal").style.display = "block";
-}
+window.addEventListener("load", function() {
 
-function closeModal() {
-  document.getElementById("myModal").style.display = "none";
-}
+    let links = document.querySelectorAll(".part a");
 
-var slideIndex = 1;
-showSlides(slideIndex);
+    links.forEach(function(link){
+        link.setAttribute("data-lightbox", "gallery");
+    });
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+});
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
